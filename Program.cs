26e8f53dd8b6546afcd32e8023084a1a7e034ef6
etb <168241334+0xetb@users.noreply.github.com>
2024-05-16 15:29:45 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ClickableTransparentOverlay;
+using ImGuiNET;
+using System.Net.Http.Headers;
+using System.Numerics;
+
+namespace BasicImGui
+{
+    class Program : Overlay
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+        protected override void Render()
+        {
+            ImGui.Begin("C# ImGui !");
+            ImGui.SetWindowSize(new Vector2(500, 500));
+            ImGui.SetCursorPos(new Vector2(250, 250));
+            ImGui.Text("Made by 0xetb");
+        }
+    }
+}
